0c71f4696d50be40bb9621a1c5b4d902e440a5f7
Sebastian <juanpinedaquiroga06@gmail.com>
2025-11-13 18:32:51 -0500

    Guardando ajustes finales del proyecto

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJuC0TRO5q-RRYW4nRKouKYzK-_TJh5BU",
-  authDomain: "taskmanagerapp-a52dc.firebaseapp.com",
-  projectId: "taskmanagerapp-a52dc",
-  storageBucket: "taskmanagerapp-a52dc.firebasestorage.app",
-  messagingSenderId: "723624971593",
-  appId: "1:723624971593:web:757cb3c40a60b3bbd79e98"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
